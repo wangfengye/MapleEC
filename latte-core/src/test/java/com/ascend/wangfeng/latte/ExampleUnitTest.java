f8ce5a13445d01fe75db66a770fe31536b444df4
wangfengye <1040441325@qq.com>
2018-03-16 14:55:22 +0800

    图表

--- latte-core/src/test/java/com/ascend/wangfeng/latte/ExampleUnitTest.java
+++ latte-core/src/test/java/com/ascend/wangfeng/latte/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.ascend.wangfeng.latte;
 
+import com.alibaba.fastjson.JSON;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +15,33 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void testGson() throws Exception {
+        Demo demo = new Demo(0.02);
+        System.out.println(JSON.toJSONString(demo));
+
+    }
+    public class Demo{
+        private double x;
+
+        public Demo(double x) {
+            this.x = x;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        @Override
+        public String toString() {
+            return "Demo{" +
+                    "x=" + x +
+                    '}';
+        }
     }
 }
